b8eff34e4f301b17f1fa54b1c7a3d8a1bb7c1cb7
Rutuja Jadhav <rutujajadhav207@gmail.com>
2023-12-12 13:24:04 +0530

    first commit

--- data-manipulation/src/App.js
+++ data-manipulation/src/App.js
@@ -1,76 +1,23 @@
-import { useState } from 'react';
-import FormComponent from './components/FormComponent';
-import TableComponent from './components/TableComponent';
-import { Button } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
-  const [ formOpen , setFormOpen ] = useState(false);
-  const [ data ,setData ] = useState([
-    {username:"Rutuja",address:"Pune",mobileNo:"12345789",status:"activate"}
-  ]);
-  const [ dataToEdit ,setDataToEdit ] = useState(null);
-
-  const handleEditData = (id) => {
-    setDataToEdit(id);
-    setFormOpen(true);
-  }
-
-  const handleDeleteData = (targetid) => {
-    setData(data.filter((_,id) => id !== targetid));
-  }
-
-  const handleSubmit = (newdata) => {
-    dataToEdit == null ?
-    setData([...data,newdata]) :
-    setData(data.map((currdata,id) => {
-      if(id !== dataToEdit){
-        return currdata ;
-      }
-      return newdata ;
-    }))
-  }
-
-  const handleSaveData = () =>{
-    localStorage.setItem("Studentdata",JSON.stringify(data))
-  }
-
-  const handleGetData = () => {
-    let d = localStorage.getItem("Studentdata")
-    console.log("typeOf data..",typeof d)
-    let objData=JSON.parse(d)
-    setData(objData)
-    console.log("objData..",objData)
- }
-
- const handleDeleteAllData = () =>{
-  localStorage.clear();
-}
-
   return (
     <div className="App">
-
-      <TableComponent data={data} deleteData={handleDeleteData} editData={handleEditData}/>
-      <Button variant="danger" onClick={() => setFormOpen(true)}>Add+</Button>
-      {
-        formOpen  && 
-        <FormComponent closeForm ={ () => {   
-          setFormOpen(false);
-          setDataToEdit(null);
-        }} 
-        onsubmit={handleSubmit}
-        defaultValue={dataToEdit !== null && data[dataToEdit]}
-        /> 
-      }
-
-      <div className='flex'>
-      <Button variant="success" onClick={handleSaveData}>SaveData_LS</Button>
-      <Button variant="warning" onClick={handleGetData}>GetData_LS</Button>
-      <Button variant="danger" onClick={handleDeleteAllData}>DeleteAllData_LS</Button>
-      </div>
-
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
